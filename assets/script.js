d356bf64db7641fff2e14ccd98c869c3e667241f
Behnam Azizi <bh.azizi@gmail.com>
2021-10-03 22:32:38 +0330

    sw

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,23 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js", {
-        scope: "/"
-    }).then(function (reg) {
-        if (reg.installing) {
-            console.log("Service worker installing");
-        } else if (reg.waiting) {
-            console.log("Service worker installed");
-        } else if (reg.active) {
-            console.log("Service worker active");
-        }
-    }).catch( error => {
-        console.log("Registration failed with " + error);
-    });
- }
+document.addEventListener('DOMContentLoaded', () => {
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js', {scope: '/Book-Cover-Template/'})
+        .then(function (reg) {
+            if (reg.installing) {
+                console.log('Service worker installing');
+            } else if (reg.waiting) {
+                console.log('Service worker installed');
+            } else if (reg.active) {
+                console.log('Service worker active');
+            }
+        })
+        .catch(function (error) {
+            console.log('Registration failed with ' + error);
+        });
+    }
+
+
+})
  
 const elm = document.querySelector("#bookCoverTemplate");
 const Base64 = {
--- sw.js
+++ sw.js
@@ -1,16 +1,38 @@
-self.addEventListener("install", (e) => {
-   e.waitUntil(
-       caches.open("v1").then((cache) => cache.addAll([
-           "/",
-           "/index.html",
-           "/assets/reset.css",
-           "/assets/style.css",
-           "/assets/script.js",
-       ]))
-   )
+const CacheName = '22_29_03_Oct_21';
+const assets = [
+    '/Book-Cover-Template/',
+    '/Book-Cover-Template/index.html',
+    '/Book-Cover-Template/assets/reset.css',
+    '/Book-Cover-Template/assets/style.css',
+    '/Book-Cover-Template/assets/script.js'
+];
+
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
 });
-self.addEventListener("fetch", (e) => {
-   e.respondWith(
-       caches.match(e.request).then((response) => response || fetch(e.request)),
-   );
+
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CacheName)
+        .map(key => caches.delete(key))
+      );
+    }));
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
